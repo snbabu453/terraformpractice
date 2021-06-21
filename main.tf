6a163d17043859a0473ececb3d01cb69f4908f1b
your username <nagendra@working.com>
2021-06-21 23:18:13 +0530

    creating alb module for existing ec2 and vpc module

--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ module "ec2" {
   subnets        = module.vpc.public_subnets
   keypair_name   = "photosatya2"
 }
+
+
+module "alb" {
+  source       = "./alb"
+  vpc_id       = module.vpc.vpc_id
+  instance1_id = module.ec2.instance1_id
+  instance2_id = module.ec2.instance2_id
+  subnet1      = module.vpc.subnet1
+  subnet2      = module.vpc.subnet2
+}
